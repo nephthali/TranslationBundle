00f61bc396bbbcf783a3e8246f003f3b0281e89d
n.djabon <neph@cineca.it>
2017-02-22 09:35:33 +0100

    Add abstract service and set parameter in dynamic compile Pass

--- Cineca/TranslationBundle/DependencyInjection/Compiler/DynamicServiceCompilerPass.php
+++ Cineca/TranslationBundle/DependencyInjection/Compiler/DynamicServiceCompilerPass.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
 
 class DynamicServiceCompilerPass implements CompilerPassInterface
 {
@@ -13,11 +14,18 @@ class DynamicServiceCompilerPass implements CompilerPassInterface
     {
         if($container->hasParameter('cineca_translation.translation_classes.translation'))
         {
-            $translation_table = $container->getParameter('cineca_translation.translation_classes.translation');
-            //$reflector = new ReflectionClass($translation_table);
-            $definition = new Definition(get_class($translation_table));
+             $translation_table = $container->getParameter('cineca_translation.translation_classes.translation');
 
-            $container->setDefinition('cineca_translation.table', $definition);
+            $configuration = $container->setDefinition(sprintf('%cineca_translation.table.class%', $translation_table), new DefinitionDecorator('cineca_translation.table'));
+
+            if($container->hasDefinition('doctrine'))
+            {
+
+            }
+            //$reflector = new ReflectionClass($translation_table);
+            //$definition = new Definition(get_class($translation_table));
+
+            //$container->setDefinition('cineca_translation.table', $definition);
         }
     }
 }
