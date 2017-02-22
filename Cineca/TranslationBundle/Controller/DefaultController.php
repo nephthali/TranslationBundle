67b3ff5adf2351869928434fd93bafcdeedd2eb8
n.djabon <neph@cineca.it>
2017-02-22 17:13:44 +0100

    Create new route to manage translation

--- Cineca/TranslationBundle/Controller/DefaultController.php
+++ Cineca/TranslationBundle/Controller/DefaultController.php
@@ -8,6 +8,25 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        //get doctrine
+        $dm = $this->get('doctrine')->getManager();
+
+        //get the page
+        $page = $this->getRequest()->get('page');
+
+        // This Action will be need to return all translation
         return $this->render('CinecaTranslationBundle:Default:index.html.twig');
     }
+
+    /**/
+    public function newAction()
+    {}
+
+    /**/
+    public function updateAction($translation)
+    {}
+
+    /**/
+    public function deleteAction($translation)
+    {}
 }
